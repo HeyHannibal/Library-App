812babbb0838bea7d9c9c6bce23bcc334d56f7fa
Hannibal <danilaivanov.iv@gmail.com>
2021-10-20 22:01:39 +0300

    Now using classes insted of constructors

--- script.js
+++ script.js
@@ -1,112 +1,110 @@
-let myLibrary = [];
+const storeJSON = (function () {
+    const save = function () {
+        localStorage.clear()
+        localStorage.setItem('library.lib', JSON.stringify(library.lib))
+    }
+    const load = function () {
+        if (localStorage.length === 0) {
+            console.log("no local data avaliable")
+        }
+        else {
+            myLib = JSON.parse(localStorage.getItem('library.lib'));
+            myLib.forEach((item) => {
+                const oldBook = new Book(item.title, item.author, item.pages, item.read)
+                library.lib.push(oldBook)
+                oldBook.createDOMBook()
+                library.booKeep()
+            })
+        }
+    }
+    return {
+        save,
+        load
+    }
+})()
 
-if (localStorage.length === 0) {
-    console.log("no local data avaliable")
-}
-else {
-    myLib = JSON.parse(localStorage.getItem('myLibrary'));
-    myLib.forEach((item) => {
-        const oldBook = new Book(item.title, item.author, item.pages, item.readOrNot)
-        myLibrary.push(oldBook)
-    })
-}
+const library = (() => {
+    const lib = []
+    const setID = function () {
+        let booksDivs = document.getElementsByClassName('bookCard');
+        let booksD = Array.from(booksDivs)
+        booksD.forEach((item, index) => {
+            item.setAttribute('id', `${index}`)
+        })
+    }
+    const totalBooks = document.querySelector('#totalBooks');
+    const booksRead = document.querySelector('#read');
+    const booksUnread = document.querySelector('#notRead');
+    const logBooks = function () {
+        totalBooks.textContent = "Total books count: " + library.lib.length;
+        let n = 0;
+        library.lib.forEach((item) => (item.read === true) ? n = n + 1 : n = n)
+        booksRead.textContent = "Books read: " + n;
+        booksUnread.textContent = "Books not read: " + (library.lib.length - n)
+    }
+    const booKeep = () => {setID(); logBooks()}
+    return {
+        lib,
+        booKeep
+    }
+})()
 
+class Book {
+    constructor(title, author, pages, trueOr) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = trueOr
+    }
+    createDOMBook() {
+        let bookTitle = document.createElement('h1')
+        let bookAuthor = document.createElement('h3')
+        let bookPages = document.createElement('h4')
+        let bookReadLabel = document.createElement('label')
+        let bookRead = document.createElement('input')
+        let deleteBook = document.createElement('button')
+        deleteBook.setAttribute('class', 'delete')
+        bookRead.setAttribute('type', 'checkbox')
 
-
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.readOrNot = read
-    this.displayed = false
-}
-
-function addBookToLibrary(t, a, p, r) {
-    if (t.length != 0 && a.length != 0 && p.length != 0) {
-       const newBook = new Book(t, a, p, r);
-        myLibrary.push(newBook);
+        bookReadLabel.textContent = "Read or Not"
+        bookAuthor.textContent = this.author
+        bookTitle.textContent = this.title
+        bookPages.textContent = this.pages
+        bookRead.checked = this.read
+        let bookCard = document.createElement('div')
+        bookCard.setAttribute('class', 'bookCard')
+        bookCard.append(bookTitle, bookAuthor, bookPages, bookRead, bookReadLabel, deleteBook)
+        display.append(bookCard)
     }
 }
 
-const display = document.querySelector('#display')
-
-function displayBooks() {
-    myLibrary.forEach((item) => {
-        if (item.displayed) {
-            return;
-        }
-        else {
-            let displayBook = document.createElement('div');
-            let disTitle = document.createElement('h2');
-            let disAuthor = document.createElement('h3');
-            let disPages = document.createElement('h3');
-            let disRead = document.createElement('input');
-            disRead.type = 'checkbox';
-            let deleteBook = document.createElement('button')
-            disTitle.textContent = item.title;
-            disAuthor.textContent = item.author;
-            disPages.textContent = item.pages;
-            disRead.checked = (item.readOrNot === true);
-            displayBook.append(disTitle, disAuthor, disPages, disRead, deleteBook);
-            display.appendChild(displayBook);
-            deleteBook.setAttribute('class', 'deleteBook')
-            displayBook.setAttribute('class', 'bookCard')
-            item.displayed = true;
-        }
-        setID()
-    })
-}
-displayBooks();
-setID();
-
-const addBook = document.querySelector('#addBook')
-addBook.addEventListener('click', function () {
+const addBook = document.querySelector('#addBook').addEventListener('click', function () {
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
-    let read = (document.getElementById("readIt").checked) ? true : false;
-    addBookToLibrary(title, author, pages, read);
-    displayBooks()
-    store()
+    let read = document.getElementById("readIt").checked;
+    if (title.length >= 3 && author.length >= 3 && pages >= 0) {
+        let newBook = new Book(title, author, pages, read)
+        newBook.createDOMBook();
+        library.lib.push(newBook)
+        library.booKeep()
+        storeJSON.save()
+    }
 })
 
-document.querySelector('#display').addEventListener('click', e => {
-    if (e.target.className === 'deleteBook') {
-        myLibrary.splice(e.target.parentNode.id, 1)
+const deleteOrCheck = document.querySelector('#display').addEventListener('click', e => {
+    if (e.target.className === 'delete') {
+        library.lib.splice(e.target.parentNode.id, 1)
         document.getElementById(`${e.target.parentNode.id}`).remove()
-        store()                                                    
+
     }
-    setID()
     if (e.target.type === 'checkbox') {
-        myLibrary[e.target.parentNode.id].readOrNot = e.target.checked;
-        console.table(myLibrary)
-        store()
+        library.lib[e.target.parentNode.id].read = e.target.checked;
     }
+    library.booKeep()
+    storeJSON.save()
 })
 
-function setID() {
-    let booksDivs = document.getElementsByClassName('bookCard');
-    let booksD = Array.from(booksDivs)
-    booksD.forEach((item, index) => {
-        item.setAttribute('id', `${index}`)
-    })
-}
+document.onload = storeJSON.load()
 
-function store() {
-    localStorage.clear()
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-    logBooks()
-}
-
-const totalBooks = document.querySelector('#totalBooks');
-const booksRead = document.querySelector('#read');
-const booksUnread = document.querySelector('#notRead');
-function logBooks() {
-    totalBooks.textContent = "Total books count: " + myLibrary.length;
-    let n = 0;
-    myLibrary.forEach((item) => (item.readOrNot === true) ? n=n+1: n=n)
-    booksRead.textContent = "Books read: " + n;
-    booksUnread.textContent = "Books not read: " + (myLibrary.length - n);
-}
-logBooks()
 
