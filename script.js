0f340ed422f82e3c0a604b95dfd680f2711aa6cd
Hannibal <88138693+HeyHannibal@users.noreply.github.com>
2021-11-14 01:48:43 +0300

    All

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const storeJSON = (function () {
     const save = function () {
-        localStorage.clear()
         localStorage.setItem('library.lib', JSON.stringify(library.lib))
     }
     const load = function () {
-        if (localStorage.length === 0) {
-            console.log("no local data avaliable")
+        if (localStorage.getItem('hasRunBefore') === null) {
+            localStorage.setItem('hasRunBefore', 'true')
+            addBook('Heart Of Darkness','Joseph Conrad','128',false)
+            addBook('Devil In the White City','Joseph Conrad','447',true)
+            addBook('Lord Of The Rings','J.R.R Tolkien', '1178', true)
+
         }
         else {
             myLib = JSON.parse(localStorage.getItem('library.lib'));
@@ -57,40 +60,61 @@ class Book {
         this.read = trueOr
     }
     createDOMBook() {
-        let bookTitle = document.createElement('h1')
-        let bookAuthor = document.createElement('h3')
-        let bookPages = document.createElement('h4')
-        let bookReadLabel = document.createElement('label')
+        let bookTitle = document.createElement('h3')
+        let bookAuthor = document.createElement('h4')
+        let bookPages = document.createElement('h5')
         let bookRead = document.createElement('input')
         let deleteBook = document.createElement('button')
         deleteBook.setAttribute('class', 'delete')
         bookRead.setAttribute('type', 'checkbox')
-
-        bookReadLabel.textContent = "Read or Not"
+        deleteBook.textContent = 'Delete'
         bookAuthor.textContent = this.author
         bookTitle.textContent = this.title
         bookPages.textContent = this.pages
         bookRead.checked = this.read
         let bookCard = document.createElement('div')
         bookCard.setAttribute('class', 'bookCard')
-        bookCard.append(bookTitle, bookAuthor, bookPages, bookRead, bookReadLabel, deleteBook)
+        bookCard.append(bookTitle, bookAuthor, bookPages, bookRead,deleteBook)
         display.append(bookCard)
     }
 }
 
-const addBook = document.querySelector('#addBook').addEventListener('click', function () {
-    let title = document.querySelector('#title').value;
-    let author = document.querySelector('#author').value;
-    let pages = document.querySelector('#pages').value;
-    let read = document.getElementById("readIt").checked;
-    if (title.length >= 3 && author.length >= 3 && pages >= 0) {
-        let newBook = new Book(title, author, pages, read)
+
+
+let formError = document.querySelector('.error')
+function checkValidity() {
+    if (!title.checkValidity()) {
+        formError.classList.add('active')
+        formError.textContent = `Title too short, add at least ${3-title.value.length} characters`
+        return false
+    } else if (!author.checkValidity()) {
+        formError.classList.add('active')
+        formError.textContent = `Author's name must have at least 5 characters. Sorry, books by Bono aren't allowed`
+        return false
+    } else if (!pages.checkValidity()) {
+        formError.classList.add('active')
+        if (pages.validity.rangeUnderflow) formError.textContent = `A proper book should have more than 50 pages`
+        return false
+    }
+    else return true 
+}
+const addBookToDom = document.querySelector('#addBook').addEventListener('click', function () {
+    let title = document.querySelector('#title');
+    let author = document.querySelector('#author');
+    let pages = document.querySelector('#pages');
+    let read = document.getElementById("readIt");
+    let isValid = checkValidity();
+    if (isValid) {
+    addBook(title.value,author.value,pages.value,read.checked)
+    }
+})
+function addBook (title,author,pages,read) {
+    let newBook = new Book(title, author, pages, read)
         newBook.createDOMBook();
         library.lib.push(newBook)
         library.booKeep()
         storeJSON.save()
-    }
-})
+}
 
 const deleteOrCheck = document.querySelector('#display').addEventListener('click', e => {
     if (e.target.className === 'delete') {
@@ -103,6 +127,14 @@ const deleteOrCheck = document.querySelector('#display').addEventListener('click
     }
     library.booKeep()
     storeJSON.save()
+})
+
+const openMenu = document.querySelector('.openForm')
+openMenu.addEventListener('click', e=> {
+let menu = document.querySelector('.libMenu')
+if(menu.classList.contains('out')) {
+    menu.classList.replace('out','in')}
+else menu.classList.replace('in','out')
 })
 
 document.onload = storeJSON.load()
